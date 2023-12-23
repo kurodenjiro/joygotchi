7af83a8b55bc37320f181031f63cff6a5a33e7d2
Kurodenjiro <kurodenjiro@gmail.com>
2023-12-24 06:18:06 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: true,
-    // env: {
-    //     CHAIN_ID: 919,
-    //     NFT_ADDRESS:'0x294041aC4ed65f7cba6B2182C2c10193fedDB9fE',
-    //     TOKEN_ADDRESS:'0x110Ac22029AbAf5e15418B95619508cAE6f1a8Ec',
-    //     FAUCET_ADDRESS:'0x0F0d82B2BEd5Cd2b68300B8e2280a7112aAd3154',
-    //     EXPLORER_URL:'https://sepolia.explorer.mode.network'
-    //   },
 }
 
 module.exports = nextConfig
