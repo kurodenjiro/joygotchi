a90a439e8cba82cb512e5bde18c133caf56b2a71
Kurodenjiro <kurodenjiro@gmail.com>
2023-12-24 06:31:16 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
     swcMinify: true,
     env: {
-        CHAIN_ID: 919,
-        NFT_ADDRESS:'0x294041aC4ed65f7cba6B2182C2c10193fedDB9fE',
-        TOKEN_ADDRESS:'0x110Ac22029AbAf5e15418B95619508cAE6f1a8Ec',
-        FAUCET_ADDRESS:'0x0F0d82B2BEd5Cd2b68300B8e2280a7112aAd3154',
-        EXPLORER_URL:'https://sepolia.explorer.mode.network'
+        CHAIN_ID: 34443,
+        NFT_ADDRESS:'0xcE489c3aAba295d1df9cE3c7111AD01ab79A21A8',
+        TOKEN_ADDRESS:'0xcE489c3aAba295d1df9cE3c7111AD01ab79A21A8',
+        FAUCET_ADDRESS:'0x2F3AE88cF1a5aa86b151Bc8879AF3f7DAFF4b4A0',
+        EXPLORER_URL:'https://explorer.mode.network'
       },
 }
 
